5cef431d21fe46ec3e13522d9b1dccb2fb72f05d
srinath ragalla <srinathragalla17@gmail.com>
2021-12-14 23:47:14 +0530

    comparision operators

---
+++ comp optrs.js
@@ -0,0 +1,57 @@
+if(10== "10"){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+if(6=== "6"){            
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+if(5 != "5"){
+    console.log(true);
+}else{
+    console.log(false);
+}
+if("9" !== "9"){                
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+if(9 < "5"){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+
+if(3 <="3"){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+if(6 > "3"){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+
+if(5 >= "5"){
+    console.log(true);
+}else{
+    console.log(false);
+}
+
+
+if(20>40){
+    console.log("wrong");
+} else if(20<40){
+   console.log("right");
+}else {
+    console.log("choose another");
+}
